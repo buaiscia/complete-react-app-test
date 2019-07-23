0a6fcdad796b840a96b55bfcb8e24d87fcbb83ea
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-07-23 15:53:20 +0200

    add switch handler button

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Babiu', age: 34}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log('was clicked');
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a create app</h1>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="30"> My hobbies: racing </Person> 
-        <Person name="Babiu" age="34" />
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: racing </Person> 
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a create app'));

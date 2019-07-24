9661c6632fe4502e74661b00ec5499272450e0e5
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-07-24 14:32:23 +0200

    add functions for changeHandler and bind funcs

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
       // this.state.persons[0] = 'Maximilian';  // DON'T DO THIS
       this.setState({
         persons: [
-          { name: 'Maximilian', age: 28},
+          { name: newName, age: 28},
           { name: 'Manu', age: 29},
           { name: 'Babiu', age: 40}
         ]
@@ -23,14 +23,33 @@ class App extends Component {
       })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28},
+        { name: event.target.value, age: 29},
+        { name: 'Babiu', age: 40}
+      ]
+      
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a create app</h1>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies: racing </Person> 
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('Maximiliannnn')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'MaximiliMaxan')}
+          changed={this.nameChangeHandler}> My hobbies: racing </Person> 
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a create app'));

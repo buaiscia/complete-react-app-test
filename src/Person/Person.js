ecabb3e661fefee713d27e62bc4f6996ff35cdc8
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-09-14 12:44:32 +0200

    fixed packages vuln. + some comment

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ const person = (props) => {
     return (
         <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I'm {props.age} years old</p>
-            <p>{props.children}</p>
+            <p>{props.children}</p>  {/* props.children is all that inside opening and closing tag of component */}            
             <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
     )

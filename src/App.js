3315888c7533c025f4444bf4b918d22c1007c28f
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-10-12 20:40:57 +0200

    add conditional style on <p>

--- src/App.js
+++ src/App.js
@@ -68,7 +68,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -102,16 +103,26 @@ class App extends Component {
                   age={this.state.persons[2].age} />       */}
           </div> 
       )
-        
+        style.backgroundColor = 'red';
       
+    }
+
+    // let classes = ['red', 'bold'].join(' ');
+    let classes  = [];
+    if(this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1 ) {
+      classes.push('bold');
     }
 
     return (
       <div className="App">
         <h1>Hi, I'm a create app</h1>
+        <p className={classes.join(' ')}>This is really working</p>
         <button 
           style={style}
-          onClick={this.togglePersonsHandler}>Switch name</button>
+          onClick={this.togglePersonsHandler}>Toggle persons</button>
         
         {persons}
         

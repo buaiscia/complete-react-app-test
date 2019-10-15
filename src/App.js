3dce5522e3cef6db6d662b7225b10eafca17bd35
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-10-15 11:22:40 +0200

    add instyle in react with radium + mediaquery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -73,7 +74,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+          backgroundColor: 'lightgreen',
+          color: 'black'
+      }
     }
 
     let persons = null;
@@ -104,6 +109,10 @@ class App extends Component {
           </div> 
       )
         style.backgroundColor = 'red';
+        style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+        }
       
     }
 
@@ -117,6 +126,7 @@ class App extends Component {
     }
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a create app</h1>
         <p className={classes.join(' ')}>This is really working</p>
@@ -127,9 +137,11 @@ class App extends Component {
         {persons}
         
       </div>
+      </StyleRoot>
+
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a create app'));
   }
 }
 
-export default App;
+export default Radium(App);

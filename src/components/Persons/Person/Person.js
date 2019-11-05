66b2ec254159ac29ea42a1e658af6dc04c594bc0
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-11-05 15:49:13 +0100

    substitute aux with fragment

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './Person.module.css'
 // import Radium from 'radium';
 import Auxiliary from '../../../hoc/Auxiliary';
@@ -10,13 +10,13 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <Auxiliary>
+            <Fragment>
             // <div className={classes.Person}>
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>,
                 <p key="i2">{this.props.children}</p>,  /* props.children is all that inside opening and closing tag of component */            
                 <input  key="i3"type="text" onChange={this.props.changed} value={this.props.name}></input>
             // </div>
-            </Auxiliary>
+            </Fragment>
         )}
 }
     // const style = {

e7b6f44ecd187d26e3aa4eeccb0100c745be8916
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-11-05 15:31:25 +0100

    add high order component to wrap person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.module.css'
 // import Radium from 'radium';
+import Auxiliary from '../../../hoc/Auxiliary';
 
 
 // const person = (props) => {
@@ -8,14 +9,15 @@ import classes from './Person.module.css'
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
-        return [
+        return (
+            <Auxiliary>
             // <div className={classes.Person}>
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>,
                 <p key="i2">{this.props.children}</p>,  /* props.children is all that inside opening and closing tag of component */            
                 <input  key="i3"type="text" onChange={this.props.changed} value={this.props.name}></input>
             // </div>
-        ]
-    }
+            </Auxiliary>
+        )}
 }
     // const style = {
     //     '@media (min-width: 500px)': {

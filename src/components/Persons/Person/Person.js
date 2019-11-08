d4d7095e8f6a2b5f0a04e996b4cad29431e6b657
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2019-11-08 11:27:50 +0100

    add ref to person js input

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,6 +9,18 @@ import PropTypes from 'prop-types';
 // const person = (props) => {
 
 class Person extends Component {
+
+    constructor(props) {
+        super(props);
+        this.inputElRef = React.createRef();
+    }
+
+
+    componentDidMount() {
+        // this.inputEl.focus();  // as mount after rendering it focus on the last loaded element
+        this.inputElRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...')
         return (
@@ -16,7 +28,14 @@ class Person extends Component {
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
                 <p key="i2">{this.props.children}</p>   
                 {/* props.children is all that inside opening and closing tag of component              */}
-                <input  key="i3"type="text" onChange={this.props.changed} value={this.props.name}></input>
+                <input  
+                    key="i3" 
+                    // ref={(inputEl) => {this.inputEl = inputEl}}  //references, to point to objects
+                    ref={this.inputElRef}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name}>
+                </input>
             </Auxiliary>
         )}
 }
